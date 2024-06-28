7d8f7e50442d71f79383f5c3fb0bfb42b66daddd
krisdimitrov <dimitkris@gmail.com>
2024-06-28 12:38:13 +0300

    Update dependencies and refactor UI code for SDL2 integration

--- chip8_ui/src/main.rs
+++ chip8_ui/src/main.rs
@@ -1,62 +1,87 @@
 use chip8_core::*;
-use std::io::{self, Write};
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::{event::Event, pixels::Color};
 
+const SCALE: u32 = 15;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 const TICKS_PER_FRAME: u32 = 10;
 
-fn main() -> io::Result<()> {
+fn main() {
     let rom_path = "roms/1-chip8-logo.ch8";
+
+    // Setup SDL
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Chip-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    // Prepare emulator and load ROM
     let mut _chip8 = Chip8::new();
-
     _chip8.load_rom(&rom_path);
-    emulator_loop(&mut _chip8)
-}
 
-fn emulator_loop(chip: &mut Chip8) -> io::Result<()> {
-    let position = term_cursor::get_pos().unwrap();
-    let x = position.0;
-    let y = position.1 - 1;
-    let mut op_counter = 0;
-
-    while op_counter < chip.op_codes_length {
-        clear_screen();
-        term_cursor::set_pos(x, y).expect("Position to be set.");
-
-        for _ in 0..TICKS_PER_FRAME {
-            chip.tick();
-        }
-
-        print_screen(&chip.screen);
-
-        op_counter += 1;
-
-        // Sleep for a short duration to simulate frame delay (optional)
-        std::thread::sleep(std::time::Duration::from_millis(500));
-    }
-
-    Ok(())
-}
-
-pub fn clear_screen() {
-    term_cursor::clear().expect("To clear console.");
-    io::stdout().flush().unwrap(); // Ensure the clear command is executed immediately
-}
-
-pub fn print_screen(screen: &[bool; 64 * 32]) {
-    let mut output = String::new();
-
-    for y in 0..crate::SCREEN_HEIGHT {
-        for x in 0..crate::SCREEN_WIDTH {
-            let index = y * crate::SCREEN_WIDTH + x;
-            if screen[index] {
-                output.push(' ');
-            } else {
-                output.push('█');
+    // Run emualtor loop
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'emulator_loop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'emulator_loop;
+                }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
+                    //
+                }
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    //
+                }
+                _ => (),
             }
         }
 
-        output.push('\n');
-    }
+        for _ in 0..TICKS_PER_FRAME {
+            _chip8.tick();
+        }
 
-    print!("{}", output); // Print the entire screen buffer
-    io::stdout().flush().unwrap();
+        draw_screen(&_chip8, &mut canvas);
+    }
+}
+
+fn draw_screen(emu: &Chip8, canvas: &mut Canvas<Window>) {
+    // Clear canvas as black
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen_buf = emu.screen;
+    // Now set draw color to white, iterate through each point and see if it should be drawn
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            // Convert our 1D array's index into a 2D (x,y) position
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+    canvas.present();
 }
